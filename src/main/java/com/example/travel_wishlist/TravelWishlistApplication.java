68a8a0ed83ac0362540f8844e91fe0db9388e8c4
isabellaTroncoso <isabella.troncoso@stud.sti.se>
2025-10-22 15:08:18 +0200

    Initial commit - Spring Boot backend setup

---
+++ src/main/java/com/example/travel_wishlist/TravelWishlistApplication.java
@@ -0,0 +1,13 @@
+package com.example.travel_wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelWishlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelWishlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/travel_wishlist/TravelWishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travel_wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelWishlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
